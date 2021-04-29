23c7456a6a5a978ec574c4aa09bda54af70362c4
Ethan Newton <EthanN0000@Gmail.com>
2021-04-29 03:36:53 -0400

    Added a new println statement.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a github test program.");
+        System.out.println("This is a github test.");
+        System.out.println("I have added more to this program.");
     }
 }
